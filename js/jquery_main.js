694381296ad17767b9a4d56220d4fb110d20d4fd
Seonghak Kim <ksh1994kr@gmail.com>
2020-12-01 02:32:43 +0900

    Link new page

--- js/jquery_main.js
+++ js/jquery_main.js
@@ -1,19 +1,43 @@
+/*var hoverMenu = $(".hidden-menu"),
+    hoverSpace = $(".site-header");
 
-//Show all menu
-$(".main_menu").css('display', 'none');
+hoverSpace.on('mousemove', function(event) {
+    if(35 - event.clientY < 0) {
+        hoverMenu.css({top: 35 - event.clientY});
+    } else {
+        hoverMenu.css({top: 0});
+    }
+}).on('mouseout', function() {
+    hoverMenu.css({top: -35});
+});*/
 
-	$(".menu_min").hover(function(){
-		if($("#hiddenMenu").val() == "up"){
-			$(".main_menu").slideDown("500");
+/*$(document).mousemove(function(e){
+  var vertical = e.pageY;
+
+  if(vertical <= 2 && !$(".hidden-menu").is(":visible")) {      
+    $(".hidden-menu").css("top","0").fadeIn();
+  } else {
+    $(".hidden-menu").css("top","-270px");
+  }
+});*/
+
+// 메뉴 전체보기
+/*	$(".hidden-menu").css('display', 'none');
+
+
+	$(".hidden-menu").hover(function(){
+		if($("#slidehid").val() == "up"){
+			$(".hidden-menu").slideDown("500");
 		}
 	}, function(){
-		$("#hiddenMenu").val("down");
-		$(".main_menu").slideUp("500",function (){
-			$("#hiddenMenu").val("up");
+		$("#slidehid").val("down");
+		$(".hidden-menu").slideUp("500",function (){
+			$("#slidehid").val("up");
 		});
 
 	});
 
 	$(".menuClose").click(function(){
-		$(".main_menu").slideUp("500");
+		$(".hidden-menu").slideUp("500");
 	});
+*/
---
+++ js/menu_link.js
@@ -0,0 +1,9 @@
+function subLink(num) {
+	
+  var linkurls = new Array;
+  
+  linkurls[1] = '../YSTool/index.html'; //main menu
+  linkurls[2] = '../YSTool/Info.html'; //company info
+  
+  window.location = linkurls[num];
+}
