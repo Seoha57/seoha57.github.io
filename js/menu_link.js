749a226eeb3d64b89c106613d7d4b0fafaff520f
SeonghakKim <ksh1994kr@gmail.com>
2020-12-03 14:40:39 +0900

    Logo Implement

--- js/menu_link.js
+++ js/menu_link.js
@@ -4,6 +4,8 @@ function subLink(num) {
   
   linkurls[1] = '../YSTool/index.html'; //main menu
   linkurls[2] = '../YSTool/Info.html'; //company info
+  linkurls[3] = '../YSTool/Products.html'; //products info
+  linkurls[5] = '../YSTool/Checkout.html'; //checkout page
   
   window.location = linkurls[num];
 }
