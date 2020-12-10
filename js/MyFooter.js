4e137fb5ce1f9a27fa469fa3d96b8cd2d156dc5f
SeonghakKim <ksh1994kr@gmail.com>
2020-12-10 19:51:13 +0900

    Re-design Footer
    Make looks good

--- js/MyFooter.js
+++ js/MyFooter.js
@@ -5,54 +5,51 @@ class MyFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <style>
+        .site-footer {
+          width: 100%;
+          height: 120px;
+          display: flex;
+          justify-content: center;
+          padding: 0 15px;
+          margin-top: 30px
+        }
+		
+		.site-footer ul {
+	      list-style-type: none;
+		}
+        
+        .site-footer .f-wrap {
+          width: 960px;
+          height: 70px;
+        }
+        
+        .site-footer .f-wrap a {
+          display: inline-block;
+          font-size: 15px
+        }
       </style>
-      <footer class="container py-5">
-        <div class="row">
-          <div class="col-12 col-md">
-            
-            <img src="../images/yslogo.png" alt="YStools Logo" width="77" height="48" >
-            <small class="d-block mb-3 text-muted">&copy; 2017-2020</small>
-          </div>
-          <div class="col-6 col-md">
-            <h5>Features</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Cool stuff</a></li>
-              <li><a class="text-muted" href="#">Random feature</a></li>
-              <li><a class="text-muted" href="#">Team feature</a></li>
-              <li><a class="text-muted" href="#">Stuff for developers</a></li>
-              <li><a class="text-muted" href="#">Another one</a></li>
-              <li><a class="text-muted" href="#">Last time</a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Resource</a></li>
-              <li><a class="text-muted" href="#">Resource name</a></li>
-              <li><a class="text-muted" href="#">Another resource</a></li>
-              <li><a class="text-muted" href="#">Final resource</a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Business</a></li>
-              <li><a class="text-muted" href="#">Education</a></li>
-              <li><a class="text-muted" href="#">Government</a></li>
-              <li><a class="text-muted" href="#">Gaming</a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md">
-            <h5>About</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Team</a></li>
-              <li><a class="text-muted" href="#">Locations</a></li>
-              <li><a class="text-muted" href="#">Privacy</a></li>
-              <li><a class="text-muted" href="#">Terms</a></li>
-            </ul>
-          </div>
-        </div>
-        <p class="row-sm-3 text-center text-muted">copyrights&copy;2020 YSTools. all right reserved</p>
+      <footer class="site-footer py-2">
+        <ul>
+          <li>
+            <div class="f-wrap row">
+              <div class="col-sm-3 py-2">
+                <img src="../images/yslogo.png" alt="YStools Logo" width="77" height="48">
+                <small class="d-block mb-3 text-muted">&copy; 2017-2020</small>
+              </div>
+              <div class="col-sm-6 text-muted py-2">
+                <h5>위치</h5>
+      		    <a>41556 대구광역시 북구 노원동로6길 2 (노원동2가)3</a>
+              </div>
+              <div class="col-sm-3 text-muted py-2">
+                <h5>대표전화</h5>
+      		    <a>053)358-2013~4</a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <p class="row-sm-3 text-center text-muted">copyrights&copy;2020 YSTools. all right reserved</p>
+          </li>
+        </ul>
       </footer>
     `;
   }
